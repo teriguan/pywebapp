94fbaaf4a41faa0f2df4d369353053599bfa2ff9
Teresa Guan <teriguan@gmail.com>
2017-11-05 21:42:54 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$(aws ecr get-login --region us-west-2)
+$(aws ecr get-login --no-include-email --region us-west-2)
 docker pull 667002880885.dkr.ecr.us-west-2.amazonaws.com/teriguan/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
